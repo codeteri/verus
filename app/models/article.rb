f65a9dc4fe2e35d5f8012bd1182688fce019fffb
WAVYTERRY <codeeeterry@gmail.com>
2023-11-28 16:57:06 +0200

    created the models and wrote down a few routes
    changed the branch default to main from master

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,14 @@
 class Article < ApplicationRecord
-  has_many :comments
-  has_many :bookmarks
-  has_many :votes
+  has_many :comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :votes, dependent: :destroy
+
+  validates :author, presence: true
+  validates :source, presence: true
+  validates :leaning, presence: true
+  validates :content, presence: true
+  validates :likes, numericality: { only_integer: true, 
+                                    greater_than_or_equal_to: 0 }
+  validates :dislikes, numericality: { only_integer: true, 
+                                    greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,24 @@ class User < ApplicationRecord
   has_many :comments
   has_many :bookmarks
   has_many :votes
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email address' }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }
+  validates :karma_score, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validates :password, presence: true, length: { minimum: 8 }, if: :password_required?
+  validates :password_confirmation, presence: true, if: :password_required?
+
+  before_validation :initialize_karma_score
+
+
+  private
+  def password_required?
+    new_record? || password.present?
+  end
+
+
+  def initialize_karma_score
+    self.karma_score ||= 0
+  end
 end
