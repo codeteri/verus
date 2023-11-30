b0f1b119b5a8ae04fa6c2f04e443fac2ab774a7f
Jack Barham <jack@barhamfamily.com>
2023-11-30 16:20:53 +0200

    new dropdown links and profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :authenticate_user!
 
-  def home
+  def dashboard
+    @view = params[:view] || 'default'
+
+    respond_to do |format|
+      format.html do
+        if request.headers["Turbo-Frame"]
+          render partial: "pages/#{@view}", layout: false
+        else
+          render :dashboard
+        end
+      end
+    end
   end
 end
