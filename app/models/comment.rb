771220ace47072ec66687186271f7ac74cfe7782
Alex Wunderlich <wunderlich.ap@gmail.com>
2023-12-05 11:01:44 +0100

    Comments working, still working on likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,16 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article
   validates :content, presence: true, length: { minimum: 2 }
-  validates :user_id, presence: true
-  validates :article_id, presence: true
+  # validates :user_id, presence: true
+  # validates :article_id, presence: true
+
+  def add_like
+    @likes = self.likes
+    @likes += 1
+  end
+
+  def add_dislike
+    @dislikes = self.dislikes
+    @dislikes += 1
+  end
 end
